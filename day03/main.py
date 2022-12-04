e6cb0bff66f1ccf1cfd5b68b347476883e085c6d
Henrik Finsberg <henrikfinsberg@hotmail.com>
2022-12-04 14:56:52 +0100

    Day3 first part in python

---
+++ day03/main.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+here = Path(__file__).absolute().parent
+
+
+def compute_priority(item: str) -> int:
+    if item.isupper():
+        return ord(item) - 65 + 27
+    else:
+        return ord(item) - 96
+
+
+def rucksack2priority(rucksack: str) -> int:
+    fst = set(rucksack[: len(rucksack) // 2])
+    snd = set(rucksack[len(rucksack) // 2 :])
+    common_item = (fst & snd).pop()
+    return compute_priority(common_item)
+
+
+def compute_total(text: str) -> int:
+    clean_text = list(
+        filter(lambda x: x != "", map(str.strip, text.strip().split("\n")))
+    )
+    return sum(map(rucksack2priority, clean_text))
+
+
+def main() -> int:
+
+    with open(here / "input.txt") as f:
+        text = f.read()
+
+    print(f"Part 1:\n{compute_total(text)}")
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
+else:
+    import pytest
+
+    example_input = """
+    vJrwpWtwJgWrhcsFMMfFFhFp\n
+    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n
+    PmmdzqPrVvPwwTWBwg\n
+    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n
+    ttgJtRGJQctTZtZT\n
+    CrZsJsPPZsGzwwsLwLmpwMDw
+    """
+
+    @pytest.mark.parametrize(("text", "expected_total"), ((example_input, 157),))
+    def test_compute_total_part1(text, expected_total) -> None:
+        assert compute_total(text) == expected_total
