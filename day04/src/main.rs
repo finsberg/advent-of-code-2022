b03ec1526e961097aaec0460e6cf402b08c5af5a
Henrik Finsberg <henrikfinsberg@hotmail.com>
2022-12-11 19:35:49 +0100

    Day 4 part2 in rust

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,26 +1,38 @@
 use std::fs;
 
-fn a_contained_in_b(a_low: &str, a_high: &str, b_low: &str, b_high: &str) -> bool {
+fn a_fully_contained_in_b(a_low: &str, a_high: &str, b_low: &str, b_high: &str) -> bool {
     let low = a_low.parse::<i32>().unwrap() >= b_low.parse::<i32>().unwrap();
     let high = a_high.parse::<i32>().unwrap() <= b_high.parse::<i32>().unwrap();
-    high & low
+    high && low
 }
 
-fn section_fully_contained(section: &str) -> u32 {
+fn a_partly_contained_in_b(a_low: &str, a_high: &str, b_low: &str, b_high: &str) -> bool {
+    let b0 = b_low.parse::<i32>().unwrap();
+    let b1 = b_high.parse::<i32>().unwrap();
+    let a0 = a_low.parse::<i32>().unwrap();
+    let a1 = a_high.parse::<i32>().unwrap();
+
+    let low = b0 <= a0 && a0 <= b1;
+    let high = b0 <= a1 && a1 <= b1;
+    high || low
+}
+
+fn section_contained(contained_func: fn(&str, &str, &str, &str) -> bool, section: &str) -> u32 {
     let (fst, snd) = section.split_once(",").unwrap();
     let (fst_low, fst_high) = fst.split_once("-").unwrap();
     let (snd_low, snd_high) = snd.split_once("-").unwrap();
 
-    (a_contained_in_b(fst_low, fst_high, snd_low, snd_high)
-        | a_contained_in_b(snd_low, snd_high, fst_low, fst_high)) as u32
+    (contained_func(fst_low, fst_high, snd_low, snd_high)
+        | contained_func(snd_low, snd_high, fst_low, fst_high)) as u32
 }
 
-fn compute_total_part1(text: &str) -> u32 {
+fn compute_total(text: &str, contained_func: fn(&str, &str, &str, &str) -> bool) -> u32 {
+    let func = |x| section_contained(contained_func, x);
     text.trim()
         .split("\n")
         .map(|x| x.trim())
         .filter(|x| !x.is_empty())
-        .map(section_fully_contained)
+        .map(func)
         .sum()
 }
 fn main() {
@@ -28,8 +40,10 @@ fn main() {
     println!("In file {}", file_path);
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    let score_part_1 = compute_total_part1(&contents);
+    let score_part_1 = compute_total(&contents, a_fully_contained_in_b);
     println!("Part1: {score_part_1}");
+    let score_part_2 = compute_total(&contents, a_partly_contained_in_b);
+    println!("Part2: {score_part_2}");
 }
 
 #[cfg(test)]
@@ -47,7 +61,13 @@ mod tests {
 
     #[test]
     fn test_compute_total_part1() {
-        let total = compute_total_part1(EXAMPLE_INPUT);
+        let total = compute_total(EXAMPLE_INPUT, a_fully_contained_in_b);
         assert_eq!(total, 2);
+    }
+
+    #[test]
+    fn test_compute_total_part2() {
+        let total = compute_total(EXAMPLE_INPUT, a_partly_contained_in_b);
+        assert_eq!(total, 4);
     }
 }
